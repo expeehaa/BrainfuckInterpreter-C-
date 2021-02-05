3453901455ee3a7e2ce04bffd3cd1653a58af2c5
expeehaa <expeehaa@outlook.com>
2021-02-05 01:36:18 +0100

    Factor interpretation out into class BrainfuckInterpreter.Interpreter.

---
+++ BrainfuckInterpreter/Interpreter.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace BrainfuckInterpreter {
+	public class Interpreter {
+		public string Code  { get; }
+		public string Input { get; }
+
+		public Interpreter(string code, string input) {
+			Code  = code;
+			Input = input;
+		}
+
+		public string Interpret(){
+			if(!string.IsNullOrWhiteSpace(Code)) {
+				var position = 0;
+				var memory = new short[0xFFFF];
+				var pointer = 0;
+				var inputPointer = 0;
+				var output = "";
+
+				int bracketsOpen, bracketsClosed;
+				while(position < Code.Length) {
+					switch(Code[position]) {
+						case '+':
+							if(memory[pointer] >= short.MaxValue) memory[pointer] = 0;
+							else memory[pointer]++;
+							break;
+						case '-':
+							if(memory[pointer] <= 0) memory[pointer] = short.MaxValue;
+							else memory[pointer]--;
+							break;
+						case '>':
+							if(pointer >= 0xFFFF) pointer = 0;
+							else pointer++;
+							break;
+						case '<':
+							if(pointer <= 0) pointer = 0xFFFF - 1;
+							else pointer--;
+							break;
+						case '.':
+							output += (char)memory[pointer];
+							break;
+						case ',':
+							memory[pointer] = (short)Input[inputPointer];
+							inputPointer++;
+							break;
+						case '[':
+							var loopEnd = position + 1;
+							bracketsOpen = 1;
+							bracketsClosed = 0;
+
+							while(loopEnd <= Code.Length && bracketsOpen != bracketsClosed) {
+								if(Code[loopEnd] == '[') {
+									bracketsOpen++;
+								} else if(Code[loopEnd] == ']') {
+									bracketsClosed++;
+								}
+
+								loopEnd++;
+							}
+
+							if(loopEnd > Code.Length) {
+								Console.WriteLine($"No end of loop found for loop start at {position}!");
+								return "";
+							}
+							if(memory[pointer] == 0) {
+								position = loopEnd;
+							}
+							break;
+						case ']':
+							var loopStart = position - 1;
+							bracketsOpen = 0;
+							bracketsClosed = 1;
+
+							while(loopStart >= 0 && bracketsOpen != bracketsClosed) {
+								if(Code[loopStart] == '[') {
+									bracketsOpen++;
+								} else if(Code[loopStart] == ']') {
+									bracketsClosed++;
+								}
+
+								loopStart--;
+							}
+
+							if(loopStart < 0) {
+								Console.WriteLine($"No start of loop found for loop end at {position}!");
+								return "";
+							}
+
+							if(memory[pointer] != 0) {
+								position = loopStart;
+							}
+
+							break;
+						default:
+							break;
+					}
+
+					position++;
+				}
+
+				return output;
+			} else {
+				return "";
+			}
+		}
+	}
+}
--- BrainfuckInterpreter/Program.cs
+++ BrainfuckInterpreter/Program.cs
@@ -25,7 +25,9 @@ namespace BrainfuckInterpreter {
 				Console.WriteLine(content);
 			}
 
-			Console.WriteLine(InterpreteCode(content, input));
+			var interpreter = new Interpreter(content, input);
+
+			Console.WriteLine(interpreter.Interpret());
 			Console.ReadLine();
 		}
 
@@ -36,101 +38,6 @@ namespace BrainfuckInterpreter {
 				return fileinfo.OpenText().ReadToEnd();
 			} catch(Exception e) {
 				Console.WriteLine(e.Message);
-				return "";
-			}
-		}
-
-		private static string InterpreteCode(string code, string input) {
-			if(!string.IsNullOrWhiteSpace(code)) {
-				var position = 0;
-				var memory = new short[0xFFFF];
-				var pointer = 0;
-				var inputPointer = 0;
-				var output = "";
-
-				int bracketsOpen, bracketsClosed;
-				while(position < code.Length) {
-					switch(code[position]) {
-						case '+':
-							if(memory[pointer] >= short.MaxValue) memory[pointer] = 0;
-							else memory[pointer]++;
-							break;
-						case '-':
-							if(memory[pointer] <= 0) memory[pointer] = short.MaxValue;
-							else memory[pointer]--;
-							break;
-						case '>':
-							if(pointer >= 0xFFFF) pointer = 0;
-							else pointer++;
-							break;
-						case '<':
-							if(pointer <= 0) pointer = 0xFFFF - 1;
-							else pointer--;
-							break;
-						case '.':
-							output += (char)memory[pointer];
-							break;
-						case ',':
-							memory[pointer] = (short)input[inputPointer];
-							inputPointer++;
-							break;
-						case '[':
-							var loopEnd = position + 1;
-							bracketsOpen = 1;
-							bracketsClosed = 0;
-
-							while(loopEnd <= code.Length && bracketsOpen != bracketsClosed) {
-								if(code[loopEnd] == '[') {
-									bracketsOpen++;
-								} else if(code[loopEnd] == ']') {
-									bracketsClosed++;
-								}
-
-								loopEnd++;
-							}
-
-							if(loopEnd > code.Length) {
-								Console.WriteLine($"No end of loop found for loop start at {position}!");
-								return "";
-							}
-							if(memory[pointer] == 0) {
-								position = loopEnd;
-							}
-							break;
-						case ']':
-							var loopStart = position - 1;
-							bracketsOpen = 0;
-							bracketsClosed = 1;
-
-							while(loopStart >= 0 && bracketsOpen != bracketsClosed) {
-								if(code[loopStart] == '[') {
-									bracketsOpen++;
-								} else if(code[loopStart] == ']') {
-									bracketsClosed++;
-								}
-
-								loopStart--;
-							}
-
-							if(loopStart < 0) {
-								Console.WriteLine($"No start of loop found for loop end at {position}!");
-								return "";
-							}
-							
-							if(memory[pointer] != 0) {
-								position = loopStart;
-							}
-
-							break;
-						default:
-							break;
-					}
-
-					position++;
-				}
-
-				return output;
-			} else {
 				return "";
 			}
 		}
